402b8624dda2b2f69988ceeef5bf8b8a8c502450
octak <xes.temp@protonmail.com>
2022-09-30 08:36:51 +0000

    Update

--- App/models/user.py
+++ App/models/user.py
@@ -1,10 +1,23 @@
-from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 class User(db.Model):
+    # Fields
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    profiles_rated = db.Column(db.Integer, nullable=False, default=0)
+    points = db.Column(db.Integer, nullable=False, default=0)
+    tier = db.Column(db.Integer, nullable=False, default=1)
+
+    times_rated = db.Column(db.Integer, nullable=False, default=0)
+    total_rating = db.Column(db.Integer, nullable=False, default=0)
+    average_rating = db.Column(db.Integer, nullable=False, default=0)
+
+    # Profile visibility information could possibly be stored in another database
+    remaining_views = db.Column(db.Integer, nullable=False, default=0)
 
     def __init__(self, username, password):
         self.username = username
@@ -24,3 +37,10 @@ class User(db.Model):
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
+    def update_tier(self):
+       self.points += 1
+
+       match self.points:
+        case 10:
+            self.tier = 10
+        
