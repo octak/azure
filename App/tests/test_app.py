bec43820d285cd55e938940d2240b458fad181e1
octak <xes.temp@protonmail.com>
2022-10-09 05:02:56 +0000

    A

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -4,14 +4,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from App.main import create_app
 from App.database import create_db
 from App.models import User
-from App.controllers import (
-    create_user,
-    get_all_users_json,
-    authenticate,
-    get_user,
-    get_user_by_username,
-    update_user
-)
+from App.controllers import *
 
 from wsgi import app
 
